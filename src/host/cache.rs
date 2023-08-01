ca2d9453720153deacaf61488df930ea6c974ef3
evan.han <iloversevan@gmail.com>
2023-08-01 14:28:31 +0800

    change cache size larger

--- src/host/cache.rs
+++ src/host/cache.rs
@@ -5,7 +5,7 @@ use std::sync::Mutex;
 
 // If maxsize is set to None, the LRU feature is disabled and the cache can grow without bound.
 // The LRU feature performs best when maxsize is a power-of-two.
-const DEFAULT_CACHE_SIZE: usize = usize::pow(2, 17);
+const DEFAULT_CACHE_SIZE: usize = usize::pow(2, 24);
 
 lazy_static::lazy_static! {
     pub static ref MERKLE_CACHE: Mutex<LruCache<String, MerkleRecord>> =
